93a96bb5c2cb72aa9fd78fc686980fcd4462d9dd
muhamad <muhamad.alinezhad.c@gmail.com>
2019-06-03 09:47:22 +0430

    (ip_updater)move code from views to checker

--- ip_updater/checker.py
+++ ip_updater/checker.py
@@ -2,11 +2,11 @@ from .updater import *
 import os
 from .models import *
 
-domain_objects = SystemDomain.objects.all()
-ip_objects = BankIP.objects.raw('SELECT * FROM dns_bank_ip WHERE domain = null')
-
 
 def domain_ping_checker():
+    domain_objects = SystemDomain.objects.all()
+    ip_objects = BankIP.objects.raw('SELECT * FROM dns_bank_ip WHERE domain = null')
+
     for domain_object in domain_objects:
         ping = os.system("ping -c 4 " + domain_object.domain)
         if ping == 0:
