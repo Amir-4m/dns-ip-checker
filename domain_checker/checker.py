d2db5e171800410c7810e14d2db4b854c6c32ae1
muhamad <muhamad.alinezhad.c@gmail.com>
2019-06-03 09:44:33 +0430

    (ip_updater)move code from views to checker

--- domain_checker/checker.py
+++ domain_checker/checker.py
@@ -4,7 +4,12 @@ import os
 
 
 def ping_and_log():
+    """
+    ping the domains and log the result as log.txt
+    :return:
+    """
     list_of_domains_to_ping = [objc.domain for objc in DomainList.objects.all()]
+
     for domain in list_of_domains_to_ping:
         ping = os.system("ping -c 4 " + domain)
         result = 'Successful' if ping == 0 else 'Unsuccessful'
