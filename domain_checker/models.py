431d508e44ac1f633dbcb46b2c5d19f7ad8a941e
muhamad <muhamad.alinezhad.c@gmail.com>
2019-06-09 13:55:59 +0430

    management commands

--- domain_checker/models.py
+++ domain_checker/models.py
@@ -11,3 +11,17 @@ class DomainList(models.Model):
 
     def __str__(self):
         return self.domain
+
+
+class DomainLog(models.Model):
+    domain = models.ForeignKey('DomainList', on_delete=models.CASCADE)
+    log_time = models.DateTimeField(auto_now_add=True)
+    latency = models.CharField(max_length=5)
+    ip = models.CharField(max_length=15)
+    is_success = models.BooleanField()
+
+    class Meta:
+        db_table = 'ping_checker_domain_log'
+
+    def __str__(self):
+        return " ".join([self.domain.domain, self.ip, self.is_success])
