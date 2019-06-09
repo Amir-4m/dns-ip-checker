fca3836b5c796b88dd92c77a0abde14f7ef5dc46
muhamad <muhamad.alinezhad.c@gmail.com>
2019-06-09 17:57:05 +0430

    TabularInline ModelAdmin

--- domain_checker/admin.py
+++ domain_checker/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from .models import DomainName, DomainPingLog
 
 
+class LogsInline(admin.TabularInline):
+    model = DomainPingLog
+    extra = 3
+
+
 class DomainNameAdmin(admin.ModelAdmin):
     list_display = ('domain', 'created_time', 'updated_time')
     list_filter = ['domain', 'created_time', 'updated_time']
+
+    inlines = [LogsInline]
 
 
 class DomainPingLogAdmin(admin.ModelAdmin):
