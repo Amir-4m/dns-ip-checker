b41e0063080008d253faec2371b8201004f5a603
muhamad <muhamad.alinezhad.c@gmail.com>
2019-06-11 16:13:38 +0430

    create dns records and update domain objects

--- ip_updater/management/commands/create.py
+++ ip_updater/management/commands/create.py
@@ -30,26 +30,30 @@ class Command(BaseCommand):
             if domain_object.domain not in list(
                     map(lambda dns: dns['name'][: dns['name'].index('.exmple.com')], dns_domains)):
                 ip = domain_object.bankip_set.first()
-                try:
-                    data = {
-                        "type": "A",
-                        "name": domain_object.domain,
-                        "content": ip if ip != None else "127.0.0.1",
-                        "ttl": 1,
-                        "proxied": False,
-                    }
+                
+                data = {
+                    "type": "A",
+                    "name": domain_object.domain,
+                    "content": ip if ip != None else "127.0.0.1",
+                    "ttl": 1,
+                    "proxied": False,
+                }
 
-                    response = requests.post(
-                        f"https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records",
-                        headers=headers,
-                        data=json.dumps(data),
-                    )
+                response = requests.post(
+                    f"https://api.cloudflare.com/client/v4/zones/{ZONE_ID}/dns_records",
+                    headers=headers,
+                    data=json.dumps(data),
+                )
 
-                    response_data = response.json()['result']
-                    print(response_data)
-                    domain_object.dns_record = response_data['id']
-                    domain_object.save()
-                    print('DONE!')
-
-                except AttributeError:
-                    pass
+                response_data = response.json()['result']
+                print(response_data)
+                domain_object.dns_record = response_data['id']
+                domain_object.save()
+                print('DONE!')
+            else:
+                for record in dns_domains:
+                    if domain_object.domain in record['name']:
+                        domain_object.dns_record = record['id']
+                        domain_object.save()
+                        print(f'{domain_object.domain} UPDATED!')
+            
