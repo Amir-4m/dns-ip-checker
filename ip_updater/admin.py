1ae75bfd91b706dc2f5893b9cf68f71b615f2f9c
muhamad <muhamad.alinezhad.c@gmail.com>
2019-06-15 10:01:29 +0430

    admin panel

--- ip_updater/admin.py
+++ ip_updater/admin.py
@@ -2,7 +2,33 @@ from django.contrib import admin
 
 from .models import BankIP, DomainLogger, DomainNameRecord, DomainZone
 
-admin.site.register(BankIP)
-admin.site.register(DomainNameRecord)
+
+class ShowLogsInline(admin.TabularInline):
+    model = DomainLogger
+
+
+class DomainRecordShowInfo(admin.ModelAdmin):
+    inlines = [ShowLogsInline]
+    exclude = ['log']
+    list_display = ['domain', 'ip', 'sub_domain_name', 'is_enable', 'created_time', 'updated_time']
+    readonly_fields = ['domain']
+
+
+class ShowSubDomainInline(admin.TabularInline):
+    model = DomainNameRecord
+
+
+class DomainZoneShowInfo(admin.ModelAdmin):
+    inlines = [ShowSubDomainInline]
+    list_display = ['domain_name', 'zone_id']
+    readonly_fields = ['domain_name', 'zone_id']
+
+
+class BankIpShowInfo(admin.ModelAdmin):
+    list_display = ['ip', 'used_time', 'created_time', 'updated_time']
+
+
+admin.site.register(BankIP, BankIpShowInfo)
+admin.site.register(DomainNameRecord, DomainRecordShowInfo)
 admin.site.register(DomainLogger)
-admin.site.register(DomainZone)
+admin.site.register(DomainZone, DomainZoneShowInfo)
