471dc94ee3f5bf180ca9d5090090be446f8c27d2
mujad <muhamad.alinejad@gmail.com>
2019-08-07 10:15:24 +0430

    optimize css of replace_ip html and move change function from admin to views

--- dns_updater/forms.py
+++ dns_updater/forms.py
@@ -11,9 +11,10 @@ class ReplaceIpForm(forms.Form):
         data = self.cleaned_data
         ip = data.get('ip')
         change_to = data.get('change_to')
+
         if not re.findall(r'\d+\.\d+\.\d+\.\d+', ip) or not re.findall(r'\d+\.\d+\.\d+\.\d+', change_to):
             raise forms.ValidationError("please insert valid ip")
-        if DomainNameRecord.objects.filter(ip=ip).count() is 0:
+        elif DomainNameRecord.objects.filter(ip=ip).count() is 0:
             raise forms.ValidationError("no such ip in DomainNameRecord database")
         else:
             return data
