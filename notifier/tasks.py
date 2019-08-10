f6f4b882a4fa4a35e223b9854f81789d25599c3b
mujad <muhamad.alinejad@gmail.com>
2019-08-10 09:48:23 +0430

    notifier tasks

--- notifier/tasks.py
+++ notifier/tasks.py
@@ -2,18 +2,20 @@
 from django.conf import settings
 from celery import shared_task
 from telegram import Bot
+from .models import NotificationMessage
 
 
 # os.environ['https_proxy'] = ''  # settings.PROXY
 
 
 @shared_task(queue='notifier')
-def send_notification(message):  # send this slug
-    send_to = message.telegramnotifier_set.all()
+def send_notification(slug):
+    notifiers = NotificationMessage.objects.get(slug=slug).telegramnotifier_set.all()
 
-    for notifier in send_to:
+    for notifier in notifiers:
+        bot = Bot(token=notifier.bot.token)
+        bot.send_message(chat_id=notifier.channel.channel_id, text=notifier.message.template)
     # token = getattr(settings, 'NOTIFIER_BOT_TOKEN', '')
     # if not token:
     #     return
-        bot = Bot(token=notifier.bot.token)
-        bot.send_message(chat_id=notifier.channel.channel_id, text=notifier.message.template)
+
