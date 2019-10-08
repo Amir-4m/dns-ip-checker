0f4975597dc3398b4a97ca92dec4d9fa4e48384c
mujad <muhamad.alinejad@gmail.com>
2019-10-08 11:02:10 +0330

    two-step verification finished

--- tel_tools/models.py
+++ tel_tools/models.py
@@ -41,6 +41,8 @@ class TelegramUser(models.Model):
     number = models.CharField(_('phone number'), max_length=13, unique=True)
     api_id = models.PositiveIntegerField(_('API ID'), unique=True)
     api_hash = models.CharField(_('API Hash'), max_length=32)
+    password = models.CharField(_('two-step verification code'), max_length=100, blank=True,
+                                help_text=_("fill if you enabled two-step verification"))
     is_enable = models.BooleanField(_('is login'), default=False, editable=False)
 
     class Meta:
